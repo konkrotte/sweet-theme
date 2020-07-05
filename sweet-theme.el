eb94a48cbcb0986a6b71e1d2d10b20098f0ef169
weebojensen <weebojensen@waifu.club>
2020-07-05 11:21:30 +0200

    Require Emacs 24.1

--- sweet-theme.el
+++ sweet-theme.el
@@ -6,6 +6,7 @@
 ;; Keywords: faces
 ;; Version: 3
 ;; URL: https://github.com/2bruh4me/sweet-theme
+;; Package-Requires: ((emacs "24.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
